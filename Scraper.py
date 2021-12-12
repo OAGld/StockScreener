1935cfd58ee59ca8f3b8837f04d04a72fe4e2d7c
OAGld <41510323+OAGld@users.noreply.github.com>
2021-12-12 17:29:39 +0100

    Add files via upload

--- Scraper.py
+++ Scraper.py
@@ -11,11 +11,17 @@ class Scrape_Data:
         self.Current_Ratio = None
         self.Shares_Outstanding = None
         self.List = []
+        self.NumberOfDividends = None
         self.YearsOfDividends = None
         self.Payout_Ratio = None
         self.D_to_E_Ratio = None
         self.CurrentYield = None
         self.TrailingYield = None
+
+        #Valuation Measures
+        self.PE_Ratio = None
+        self.MarketCap = None
+        self.PB_Ratio = None
         Scrape_Yahoo_Finance.Scrape_Stats(self)
         Scrape_Yahoo_Finance.Scrape_Annual_Dividends(self)
 
@@ -24,11 +30,44 @@ class Scrape_Yahoo_Finance(Scrape_Data):
     def Scrape_Stats(self):
         try:
             URL = 'https://finance.yahoo.com/quote/' + self.Comp_Name + '/key-statistics?p=' + self.Comp_Name
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
 
             #Parse website
-            stats_page = requests.get(URL)
+            stats_page = requests.get(URL, headers=headers)
             stats = BeautifulSoup(stats_page.content, 'html.parser')
 
+            #Find all tags containing the relevant info from the "Valuation Measures" table
+            Valuation_Measures = stats.find('div', class_="Fl(start) smartphone_W(100%) W(50%)")
+            Table_Divs = Valuation_Measures.find_all('div', class_="Pos(r) Mt(10px)")
+
+            #Extract data from the "Valuation Measures" table 
+            for a in Table_Divs:
+                try:
+                    table = a.find_all('table')
+                    for b in table:
+                        try:
+                            row = b.find_all('tr')
+                            for c in row:
+                                try:
+                                    Name = c.find('span')
+                                    Value = c.find('td', class_="Fw(500) Ta(end) Pstart(10px) Miw(60px)")
+
+                                    #Assign the values
+                                    if ("Market Cap" in Name):
+                                        self.MarketCap = float(Value.text.replace('%', ''))
+                                    if ("Trailing P/E" in Name):
+                                        self.PE_Ratio = float(Value.text.replace('%', ''))
+                                    if ("Price/Book" in Name):
+                                        self.PB_Ratio = float(Value.text.replace('%', ''))
+                                except Exception as e:
+                                    print(self.Comp_Name + ": ")
+                                    print(e)
+                        except Exception as e:
+                            print(self.Comp_Name + ": ")
+                            print(e)
+                except Exception as e:
+                    print(self.Comp_Name + ": ")
+                    print(e)
             #Find all tags containing the relevant info from the "Financial Highlights" table
             Financial_Highlights = stats.find('div', class_="Fl(start) W(50%) smartphone_W(100%)")
             Table_Divs = Financial_Highlights.find_all('div', class_="Pos(r) Mt(10px)")
@@ -55,7 +94,7 @@ class Scrape_Yahoo_Finance(Scrape_Data):
                                     if ("Current Ratio" in Name):
                                         self.Current_Ratio = float(Value.text.replace('%', ''))
                                     if ("Total Debt/Equity" in Name):
-                                        self.D_to_E_Ratio = Value.text
+                                        self.D_to_E_Ratio = float(Value.text)
                                 except Exception as e:
                                     print(self.Comp_Name + ": ")
                                     print(e)
@@ -65,6 +104,8 @@ class Scrape_Yahoo_Finance(Scrape_Data):
                 except Exception as e:
                     print(self.Comp_Name + ": ")
                     print(e)
+
+
             #Find all tags containing the relevant info from the "Trading Information" table
             Trading_Information = stats.find('div', class_="Fl(end) W(50%) smartphone_W(100%)")
             Table_Divs = Trading_Information.find_all('div', class_="Pos(r) Mt(10px)")
@@ -83,7 +124,7 @@ class Scrape_Yahoo_Finance(Scrape_Data):
 
                                     #Assign the values
                                     if ("Shares Outstanding" in Name):
-                                        self.Shares_Outstanding = Value.text
+                                        self.Shares_Outstanding = float(Value.text.replace('M', ''))
                                     if ("Payout Ratio" in Name):
                                         self.Payout_Ratio = Value.text
                                     if ("Forward Annual Dividend Yield" in Name):
@@ -114,20 +155,22 @@ class Scrape_Yahoo_Finance(Scrape_Data):
         List = []
 
         try:
-            URL = 'https://finance.yahoo.com/quote/' + self.Comp_Name + '/history?period1=1097452800&period2=1596326400&interval=div%7Csplit&filter=div&frequency=1mo'
+            URL = 'https://finance.yahoo.com/quote/' + self.Comp_Name + '/history?period1=1265328000&period2=1639180800&interval=capitalGain%7Cdiv%7Csplit&filter=div&frequency=1d&includeAdjustedClose=true'
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
 
             #Parse website
-            Historical_Data_page = requests.get(URL)
-            Historical_Data = BeautifulSoup(Historical_Data_page.content, 'html.parser')
+            Historical_Data_page = requests.get(URL, headers=headers)
 
             #Find all tags containing the relevant info
-            Dividends = Historical_Data.find('tbody')
-            row = Dividends.find_all('tr')
+            Historical_Data = BeautifulSoup(Historical_Data_page.content, 'html.parser')
+            Historical_Data = Historical_Data.find('div', class_='Pb(10px) Ovx(a) W(100%)')
+            Table = Historical_Data.find('tbody')
+            row = Table.find_all('tr', class_='BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
 
             #Collect the historical data and append to List
             for b in row:
                 try:
-                    Year = int(b.find('td', class_="Py(10px) Ta(start) Pend(10px)").text[8:]) #Strips the first 8 characters from the string, so only the year is left and converts it to an int
+                    Year = int(b.find('td', class_="Py(10px) Ta(start) Pend(5px)").text[8:]) #Strips the first 8 characters from the string, so only the year is left and converts it to an int
                     Value = float(b.find('strong').text)
                     List.append([])
                     List[Counter].append(Year)
@@ -140,7 +183,7 @@ class Scrape_Yahoo_Finance(Scrape_Data):
 
             Flag = 0
             Counter = 0
-            self.YearsOfDividends = len(List)
+            self.NumberOfDividends = len(List)
 
             #Create an annualised list with all partial dividends combined for each year
             for x in range(len(List)-1):
@@ -158,6 +201,7 @@ class Scrape_Yahoo_Finance(Scrape_Data):
                     print(self.Comp_Name + ": ")
                     print(e)
             try:
+                self.YearsOfDividends = Counter
                 #Remove data which may be incomplete or wrong from list
                 self.List.pop(0)
                 self.List.pop(len(self.List)-1)
